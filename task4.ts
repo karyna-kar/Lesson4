c2ee5c36e83c246bcf3c6f3fceccc33d12fbc13e
Karina <karyna.karmaza@gmail.com>
2026-01-28 13:12:42 +0200

    fix: resolving comments

--- task4.ts
+++ task4.ts
@@ -7,7 +7,7 @@ const order = "Order#1456; date=2026-01-26 09:07:05; amount=15.3";
 const orderNumber = order.slice(order.indexOf("#") + 1, order.indexOf(";"));
 
 //parse date
-const parsedDate = order.slice(order.indexOf("=") + 1, order.lastIndexOf(";"));
+const parsedDate = order.slice(order.indexOf("date=") + "date=".length, order.lastIndexOf(";"));
 const date = new Date(parsedDate);
 const day = date.getDate().toString().padStart(2, "0");
 const month = (date.getMonth() + 1).toString().padStart(2, "0");
@@ -16,7 +16,7 @@ const hours = date.getHours().toString().padStart(2, "0");
 const minutes = date.getMinutes().toString().padStart(2, "0");
 
 //parse order amount
-const amount = order.slice(order.lastIndexOf("=") + 1);
+const amount = order.slice(order.indexOf("amount=") + "amount=".length);
 const finalAmount = Math.ceil(Number.parseFloat(amount));
 
 console.log(`Заказ № ${orderNumber} от ${day}/${month}/${year} ${hours}:${minutes} на сумму ${finalAmount} рублей`);
