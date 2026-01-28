902b732bfdce9edb4d2cda9a650dfed1339a9bc4
Karina <karyna.karmaza@gmail.com>
2026-01-28 10:31:06 +0200

    feat: resolve task4

--- task4.ts
+++ task4.ts
@@ -1,5 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const order = "Order#1456; date=2026-01-26 09:07:05; amount=15.3";
 
 // преобразовать строку в формат:
 // Заказ № 1456 от 26/01/2026 09:07 на сумму 16 рублей
+
+//parse orderNumber
+const orderNumber = order.slice(order.indexOf("#") + 1, order.indexOf(";"));
+
+//parse date
+const parsedDate = order.slice(order.indexOf("=") + 1, order.lastIndexOf(";"));
+const date = new Date(parsedDate);
+const day = date.getDate().toString().padStart(2, "0");
+const month = (date.getMonth() + 1).toString().padStart(2, "0");
+const year = date.getFullYear().toString().padStart(2, "0");
+const hours = date.getHours().toString().padStart(2, "0");
+const minutes = date.getMinutes().toString().padStart(2, "0");
+
+//parse order amount
+const amount = order.slice(order.lastIndexOf("=") + 1);
+const finalAmount = Math.ceil(Number.parseFloat(amount));
+
+console.log(`Заказ № ${orderNumber} от ${day}/${month}/${year} ${hours}:${minutes} на сумму ${finalAmount} рублей`);
